01dc9b0f31ca6936ea7885d05ad7c6e55c483922
seoyoung7623 <seoyoung7623@naver.com>
2023-07-01 02:43:18 +0900

    initial: 초기세팅

---
+++ src/main/java/com/example/oneulnail/OneulNailApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneulnail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneulNailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneulNailApplication.class, args);
+	}
+
+}
