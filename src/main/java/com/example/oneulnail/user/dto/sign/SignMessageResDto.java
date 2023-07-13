8664947f446a2b9eb33cb416d3fd5d5878f24f5d
seoyoung7623 <seoyoung7623@naver.com>
2023-07-14 00:55:00 +0900

    feat: 핸드폰 인증문자 구현

--- src/main/java/com/example/oneulnail/user/dto/sign/SignMessageResDto.java
+++ src/main/java/com/example/oneulnail/user/dto/sign/SignMessageResDto.java
@@ -1,7 +1,6 @@
 package com.example.oneulnail.user.dto.sign;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -9,11 +8,6 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class SignInReqDto {
-
-    private String phone_num;
-
-    private String password;
-
+public class SignMessageResDto {
+    private String authenticationNumber;
 }
--- src/main/java/com/example/oneulnail/user/dto/sign/SignUpReqDto.java
+++ src/main/java/com/example/oneulnail/user/dto/sign/SignUpReqDto.java
@@ -1,5 +1,6 @@
 package com.example.oneulnail.user.dto.sign;
 
+import com.example.oneulnail.common.config.security.oauth2.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,14 +10,15 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Builder
 public class SignUpReqDto {
 
-    private String id;
+    private String phone_num;
 
     private String password;
 
     private String name;
+
+    private Role role;
 
 }
