c38d5787406de64f839f26d1280cb252edcfe563
ggamD00 <jaehuek@naver.com>
2023-07-14 16:59:12 +0900

    feat: 오타 수정 및 ResponseDTO에 @Getter 설정
    json으로 변환하려면 DTO에 @Getter가 필요하다!

--- src/main/java/com/example/oneulnail/domain/shop/controller/ShopController.java
+++ src/main/java/com/example/oneulnail/domain/shop/controller/ShopController.java
@@ -7,9 +7,11 @@ import com.example.oneulnail.global.entity.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/shop")
+@RestController
+@RequestMapping("/shop")
 @RequiredArgsConstructor
 public class ShopController {
 
