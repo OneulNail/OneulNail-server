f69a3f5854998c8c3864145626bf92f28d0f5d4b
ggamD00 <jaehuek@naver.com>
2023-07-20 03:24:00 +0900

    feat: 소셜 로그인 후 추가정보 등록하면 GUEST에서 USER로 변경

--- src/main/java/com/example/oneulnail/global/config/security/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/oneulnail/global/config/security/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -3,8 +3,6 @@ package com.example.oneulnail.global.config.security.oauth2.handler;
 import com.example.oneulnail.global.config.security.jwt.service.JwtService;
 import com.example.oneulnail.global.config.security.oauth2.entity.CustomOAuth2User;
 import com.example.oneulnail.global.config.security.oauth2.entity.Role;
-import com.example.oneulnail.domain.user.entity.User;
-import com.example.oneulnail.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -23,7 +21,6 @@ import java.io.IOException;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtService jwtService;
-    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -39,11 +36,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
                 response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
                 response.sendRedirect("/oauth2/sign-up"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
-
-                jwtService.sendAccessAndRefreshToken(response, accessToken, null);
-                User findUser = userRepository.findByEmail(oAuth2User.getEmail())
-                                .orElseThrow(() -> new IllegalArgumentException("이메일에 해당하는 유저가 없습니다."));
-                findUser.authorizeUser();
             } else {
                 loginSuccess(response, oAuth2User); // 로그인에 성공한 경우 access, refresh 토큰 생성
             }
