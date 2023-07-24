3edccea1df0cead0ff4554c1e64df340d96b2def
seoyoung7623 <seoyoung7623@naver.com>
2023-07-25 01:57:02 +0900

    feat: 상품등록 & 상품단일조회

---
+++ src/main/java/com/example/oneulnail/domain/product/dto/response/ProductRegisterResDto.java
@@ -0,0 +1,10 @@
+package com.example.oneulnail.domain.product.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ProductRegisterResDto {
+    private String msg;
+}
