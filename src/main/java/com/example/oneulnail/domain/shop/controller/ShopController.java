94d03241e8b60d40139783d59676372f7108fb62
SeoYoung <seoyoung7623@naver.com>
2023-09-03 01:42:21 +0900

    feat: shop s3 이미지 등록 추가

--- src/main/java/com/example/oneulnail/domain/shop/controller/ShopController.java
+++ src/main/java/com/example/oneulnail/domain/shop/controller/ShopController.java
@@ -5,6 +5,7 @@ import com.example.oneulnail.domain.shop.dto.response.ShopListResDto;
 import com.example.oneulnail.domain.shop.dto.response.ShopFindOneResDto;
 import com.example.oneulnail.domain.shop.dto.response.ShopRegisterResDto;
 import com.example.oneulnail.domain.shop.service.ShopService;
+import com.example.oneulnail.global.constants.S3Upload;
 import com.example.oneulnail.global.entity.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +14,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Tag(name = "네일샵")
 @RestController
@@ -21,11 +25,15 @@ import org.springframework.web.bind.annotation.*;
 public class ShopController {
 
     private final ShopService shopService;
+    private final S3Upload s3Upload;
 
     @Operation(summary = "가게 등록")
     @PostMapping
-    public BaseResponse<ShopRegisterResDto> register(@RequestBody ShopRegisterReqDto shopRegisterReqDto) {
-        ShopRegisterResDto shopRegisterResDto = shopService.register(shopRegisterReqDto);
+    public BaseResponse<ShopRegisterResDto> register(
+            @RequestPart(value = "shopRegisterReq") ShopRegisterReqDto shopRegisterReqDto,
+            @RequestPart(value = "image", required = false) MultipartFile file) throws IOException {
+        String imageUrl = s3Upload.upload(file);
+        ShopRegisterResDto shopRegisterResDto = shopService.register(shopRegisterReqDto,imageUrl);
         return BaseResponse.onSuccess(shopRegisterResDto);
     }
 
